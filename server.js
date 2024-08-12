0b82c7f23789be7e65b0aa6d6323b23c3ec8d200
Zerokyh <koreaguh@naver.com>
2024-08-12 16:25:46 +0900

    server setting update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = mysql.createPool({
   database: "studentattendance",
 });
 
-const PORT = 8001;
+const PORT = 3306;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
